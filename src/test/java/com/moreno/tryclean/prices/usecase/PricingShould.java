f44228f7a80f9421c598090f888a4cc3dcb14ea6
Nelson Hernandez <pilolay40@gmail.com>
2020-02-26 21:16:44 +0100

    Test para el calculo del item  - Calculamos itemPrice por el numberOfItems  - Refacturamos código

--- src/test/java/com/moreno/tryclean/prices/usecase/PricingShould.java
+++ src/test/java/com/moreno/tryclean/prices/usecase/PricingShould.java
@@ -3,6 +3,7 @@ package com.moreno.tryclean.prices.usecase;
 
 import com.moreno.tryclean.prices.api.dto.ItemRequest;
 import com.moreno.tryclean.prices.boundaries.PricingBoundaries;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -11,11 +12,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PricingShould {
 
+    private PricingBoundaries pricing;
+
+    @BeforeEach
+    public void setUp() {
+        pricing = new Pricing();
+    }
+
     @Test
     public void shouldReturnZeroWithPriceOrNumberOfItemIsZero() {
-        PricingBoundaries pricing = new Pricing();
+
         ItemRequest itemRequest = new ItemRequest("TV",new BigDecimal(0),0,"BAL");
-        BigDecimal expected = new BigDecimal(0);
+        BigDecimal expected = BigDecimal.ZERO;
         BigDecimal actual = pricing.calculateTotalAmount(
                 itemRequest.getItemDescription(),
                 itemRequest.getItemPrice(),
@@ -25,5 +33,16 @@ public class PricingShould {
         assertEquals(expected, actual);
     }
 
-
+    @Test
+    public void showReturnSumOfPrice() {
+        ItemRequest itemRequest = new ItemRequest("Bike",new BigDecimal(1000),2,"CAN");
+        BigDecimal expected = new BigDecimal(2000);
+        BigDecimal actual = pricing.calculateTotalAmount(
+                itemRequest.getItemDescription(),
+                itemRequest.getItemPrice(),
+                itemRequest.getNumberOfItems(),
+                itemRequest.getState()
+        );
+        assertEquals(expected, actual);
+    }
 }
